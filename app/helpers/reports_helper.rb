9221868d17e72488c2d0efe9959fa06793c2c6d0
Derek Zhao <derek.zhao@yale.edu>
2010-06-22 00:20:18 -0400

    making updating data objects look better

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,3 +1,8 @@
 module ReportsHelper
+		# Customizes the thickbox height depending on how many data fields there are
+	def link_to_update_data_object(report_data_object)
+		height = report_data_object.data_fields.length * 150
+		link_to "Update #{report_data_object.name}", new_data_object_data_entry_path(report_data_object, :height => height, :width => "500"), :title => "Update #{report_data_object.name}", :class => "thickbox", :onclick => "this.blur();"
+	end
 end
 
