6008c92ed0b6fd68fce30385a2580e1bb240d413
James Luo <James@dhcp128036203199.central.yale.edu>
2010-07-09 15:47:51 -0400

    duplication problem

---
+++ db/migrate/20100702171923_create_shifts_tasks.rb
@@ -0,0 +1,15 @@
+class CreateShiftsTasks < ActiveRecord::Migration
+  def self.up
+    create_table :shifts_tasks, :id => false do |t|
+      t.boolean :on_time
+      t.references :task
+      t.references :shift
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :shifts_tasks
+  end
+end
