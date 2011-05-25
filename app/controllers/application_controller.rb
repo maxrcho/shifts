76ba46ef5f30316c4443adcd640686bffe25275b
Adam Bray <adam.bray@yale.edu>
2011-05-25 14:38:40 -0400

    demo commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -89,7 +89,7 @@ class ApplicationController < ActionController::Base
       respond_to do |format|
         format.html do
           flash[:error] = error_message
-          redirect_to index
+          redirect_to indexa
         end
         format.js do
           render :update do |page|
