23f9097f4ffb3cebd72e947cea337c56ec787e10
Maria Altyeva <maria.altyeva@yale.edu>
2011-05-25 12:24:51 -0400

    Added payform change and addition notifications

--- app/models/app_mailer.rb
+++ app/models/app_mailer.rb
@@ -33,13 +33,22 @@ class AppMailer < ActionMailer::Base
     body        :sub_request => sub_request, :new_shift => new_shift
   end
 
-  def payform_item_change_notification(old_payform_item, new_payform_item, dept)
+  def payform_item_change_notification(new_payform_item, dept)
     recipients  new_payform_item.user.email
     from        dept.department_config.mailer_address
     sent_on     Time.now
     subject     "Your payform item has been modified by #{new_payform_item.source}"
-    body        :old_payform_item => old_payform_item, :new_payform_item => new_payform_item
+    body        :new_payform_item => new_payform_item
   end
+  
+  def payform_item_add_notification(new_payform_item, dept)
+    recipients  new_payform_item.user.email
+    from        dept.department_config.mailer_address
+    sent_on     Time.now
+    subject     "A new item was added to your payform by #{new_payform_item.source}"
+    body        :new_payform_item => new_payform_item
+  end
+  
 
   def payform_item_deletion_notification(old_payform_item, dept)
     recipients  old_payform_item.user.email
