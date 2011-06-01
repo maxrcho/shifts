3ff70df82def2c913afcb491bbda782e752e4997
Casey Watts <casey.s.watts@gmail.com>
2011-06-01 12:23:18 -0400

    added nice date/time inputs to the announcement form

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -3,16 +3,20 @@ class Notice < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
   belongs_to :remover, :class_name => "User"
   belongs_to :department
-	
-  validate :content_or_label, :presence_of_locations_and_viewers, :proper_time	 	
 
+  validate :content_or_label, :presence_of_locations_and_viewers, :proper_time
+
+  attr_accessor :start_date
+  attr_accessor :start_time
+  attr_accessor :end_date
+  attr_accessor :end_time
   #before_destroy :destroy_user_sinks_user_sources    TODO:  this validation fails, but also is never called as we never delete notices.
   named_scope :in_department, lambda { |dept| {:conditions => {:department_id => dept}}}
   named_scope :created_by, lambda { |user| {:conditions => {:author_id => user}}}
   named_scope :inactive, lambda {{ :conditions => [" end < ?", Time.now.utc] }}
   named_scope :not_link, :conditions => ["type != ?", "Link"]
   named_scope :upcoming,  lambda {{ :conditions => ["start > ? ", Time.now.utc] }}
-  
+
   def self.active_links
      Link.active
   end
@@ -49,7 +53,7 @@ class Notice < ActiveRecord::Base
     self.remover = user
     true if self.save
   end
-  
+
   def content_with_formatting
     content.sanitize_and_format
   end
@@ -65,7 +69,7 @@ class Notice < ActiveRecord::Base
   def proper_time
     errors.add_to_base "Start/end time combination is invalid." if self.end && self.start >= self.end
   end
-  
+
   def destroy_user_sinks_user_sources
     UserSinksUserSource.delete_all("#{:user_sink_type.to_sql_column} = #{"Notice".to_sql} AND #{:user_sink_id.to_sql_column} = #{self.id.to_sql}")
   end
